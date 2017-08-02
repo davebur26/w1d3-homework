8af5203f2efcab1bfc3a4abeea7f75e9a20772d2
davebur26 <davebur26@gmail.com>
2017-08-02 17:43:20 +0100

    remove the comment symbols to display answers

--- arrays_hashes_quiz.rb
+++ arrays_hashes_quiz.rb
@@ -3,75 +3,75 @@
 lines = ['Gyle Centre', 'Edinburgh Park', 'Murrayfield Stadium', 'Haymarket', 'Princes Street']
 
 # 1. Work out how many stops there are in the array
-#puts lines.length()
+puts lines.length()
 
 # 2. Return 'Edinburgh Park' from the array
 
-#return lines[1]
+return lines[1]
 #
 # # 3. How many ways can we return 'Princes Street' from the array?
 #
-# puts lines[4]
-# puts lines[-1]
-# puts lines.last
+puts lines[4]
+puts lines[-1]
+puts lines.last
 #
 # # 4. Work out the index position of 'Haymarket'
 #
-# puts lines.index('Haymarket')
+puts lines.index('Haymarket')
 #
 # # 5. Add 'Airport' to the start of the array
 #
-# lines.unshift("Airport")
-# puts lines
-# puts
+lines.unshift("Airport")
+puts lines
+puts
 # # 6. Add 'York Place' to the end of the array
 #
-# lines.push("York Place")
-# puts lines
-# puts
+lines.push("York Place")
+puts lines
+puts
 
 # 7. Remove 'Edinburgh Park' from the array using it's name
 #
-# #lines.delete("Edinburgh Park")
-# #puts lines
-# puts
-#
-#
-# # 8. Delete 'Edinburgh Park' from the array by index
-#
-# lines.delete(1)
-# puts lines
-# puts
+lines.delete("Edinburgh Park")
+puts lines
+puts
 
-# 9. Reverse the positions of the stops in the array
 
-# puts lines.reverse
+# 8. Delete 'Edinburgh Park' from the array by index
 
-### B. Given the following data structure:
-#
-# my_hash = {"0" => "Zero", 1 => "One", :two => "Two", "two" => 2}
-#
-# # 1. How would you return the string `"One"`?
-#
-# puts my_hash[1]
-#
-# # 2. How would you return the string `"Two"`?
-#
-# puts my_hash[:two]
-#
-# # 3. How would you return the number `2`?
-#
-# puts my_hash["two"]
-#
-# # 4. How would you add `{3 => "Three"}` to the hash?
-#
-# my_hash[3] = "Three"
-# puts my_hash
-#
-# # 5. How would you add `{:four => 4}` to the hash?
-#
-# my_hash[:four] = 4
-# puts my_hash
+lines.delete(1)
+puts lines
+puts
+
+#9. Reverse the positions of the stops in the array
+
+puts lines.reverse
+
+## B. Given the following data structure:
+
+my_hash = {"0" => "Zero", 1 => "One", :two => "Two", "two" => 2}
+
+# 1. How would you return the string `"One"`?
+
+puts my_hash[1]
+
+# 2. How would you return the string `"Two"`?
+
+puts my_hash[:two]
+
+# 3. How would you return the number `2`?
+
+puts my_hash["two"]
+
+# 4. How would you add `{3 => "Three"}` to the hash?
+
+my_hash[3] = "Three"
+puts my_hash
+
+# 5. How would you add `{:four => 4}` to the hash?
+
+my_hash[:four] = 4
+puts my_hash
 
 ### C. Given the following data structure:
 
